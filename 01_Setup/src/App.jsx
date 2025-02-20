29d2673e3b6f6abc33c11e6de55f4ef4e658b0e7
Ricardo Salgado <34379669+ricksalgado@users.noreply.github.com>
2025-02-20 16:51:40 -0500

    App file upload
    First app file, deleted the errors

--- 01_Setup/src/App.jsx
+++ 01_Setup/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World! Let's learn React</h1>
+      <h1>Hello World! I will learn React</h1>
     </>
   )
 }
