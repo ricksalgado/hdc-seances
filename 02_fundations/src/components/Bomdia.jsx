86b805d409d60cc6270f3e4158ec113dcbbd4d0d
Ricardo Salgado <34379669+ricksalgado@users.noreply.github.com>
2025-02-20 17:29:25 -0500

    First components

---
+++ 02_fundations/src/components/Bomdia.jsx
@@ -0,0 +1,12 @@
+const Bomdia = () => {
+
+    const name = "Rick";
+
+    return (
+
+        <h3>Bom dia {name}</h3>
+
+  )
+}
+
+export default Bomdia
---
+++ 02_fundations/src/components/Meunome.jsx
@@ -0,0 +1,9 @@
+const Meunome = () => {
+  return (
+    <p>
+      Meu nome é Ricardo Salgado. Eu sou desenvolvedor frontEnd
+    </p>
+  )
+}
+
+export default Meunome
