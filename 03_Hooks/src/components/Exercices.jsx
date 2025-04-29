40f1ee96e40b3fb3b9ab618fef70495a41acb9bb
Ricardo Salgado <34379669+ricksalgado@users.noreply.github.com>
2025-04-29 16:58:50 -0400

    3.8 Exercise 2
    Exercise 2 - useMemo.
    I had to create a componente that calculate the fibonacci sequence, in a light way.

--- 03_Hooks/src/components/Exercices.jsx
+++ 03_Hooks/src/components/Exercices.jsx
@@ -1,62 +1,74 @@
-import React from "react";
-import { useEffect } from "react";
-
+import React, { useState, useEffect, useMemo } from "react";
 
 // - Exercício 1: Uso de useEffect para Sincronização de Dados
 // Enunciado:
 // Você precisa exibir informações de um usuário que são recebidas via props em um componente.
 // Crie um componente que use o hook useEffect para sincronizar as informações do usuário com o título do documento (o título da aba no navegador).
 
-const Escalacao = ({jogador}) => {
+const Escalacao = ({ jogador }) => {
+  const title = document.title;
 
+  useEffect(() => {
+    document.title = jogador.name;
+  }, [jogador]);
 
-    
-    useEffect(() => {
-        document.title = jogador.name;
-    }, [jogador]);
-    
-
-    return (
-        <div>
-            <h1>Exercices</h1>
-            <h2>Exercicio 1</h2>
-            <p>Props Jogador: {jogador.name}</p>
-            <p>O Jogador é um: {jogador.posicao}</p>
-            <p>Title da pagina é: {document.title} </p>
-
-        </div>
-    );
-
-}
-
-
-
-const Exercices = () => {
-
-    const palmeiras = {
-        name: "Estevao",
-        posicao: "Atacante"
-    }
-
-    return(
-        <Escalacao jogador={palmeiras} />
-    )
-
+  return (
+    <div>
+      <h1>Exercices</h1>
+      <h2>Exercicio 1</h2>
+      <p>Props Jogador: {jogador.name}</p>
+      <p>O Jogador é um: {jogador.posicao}</p>
+      <p>Title da pagina é: {title} </p>
+    </div>
+  );
 };
-
-export default Exercices;
-
 
 // - Exercício 2: Memorizando Cálculos com useMemo
 // Enunciado:
 // Suponha que você tenha um componente que realiza um cálculo pesado, como uma função de fibonacci que é chamada com um número específico.
 // Use o hook useMemo para evitar que o cálculo seja refeito desnecessariamente.
 
+const Fibo = () => {
+  let [num1, setNum1] = useState(0);
+  let [num2, setNum2] = useState(1);
 
+  const result = useMemo(() => {
+    return num1 + num2;
+  }, [num1, num2]);
 
+let calcRender = () => {
+  setNum1(num2);
+  setNum2(result)
+} 
 
+  return (
+    <div>
+      <h2>Fibonacci Sequence</h2>
+      <p>Current: {num1} and {num2}</p>
+      <p>Next number is: {result}</p>
+      <button onClick={calcRender} >Next Number</button>
+    </div>
+  );
+};
 
 // - Exercício 3: Criação e Uso de um Custom Hook
 // Enunciado:
 // Crie um custom hook chamado useOnlineStatus que rastreia se o usuário está online ou offline.
 // Use este hook em um componente para exibir o status atual do usuário.
+
+// Renderiza a pg
+const Exercices = () => {
+  const palmeiras = {
+    name: "Estevao",
+    posicao: "Atacante",
+  };
+
+  return (
+    <div>
+      <Escalacao jogador={palmeiras} />
+      <Fibo />
+    </div>
+  );
+};
+
+export default Exercices;
